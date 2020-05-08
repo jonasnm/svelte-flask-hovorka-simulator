95dda958a98d289b5b1667ad1e6a0431f3c4ab02
Jonas <fluehode@gmail.com>
2020-05-08 21:56:12 +0200

    Clean up App.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -10,28 +10,30 @@
 		  return bg = d
 	  });
   }
+  
+  function loadShow() {
+	  getBg()
+	  renderChart()
+  }
 
 
 // Copied from internet!
   	import { onMount } from "svelte";
 	import Chart from "chart.js";
 	onMount(async () => {});
-	function renderChart(bg) {
+	function renderChart() {
 		var ctx = document.getElementById("myChart").getContext("2d");
-		let x = [...Array(10).keys()]
 		var chart = new Chart(ctx, {
 		type: "line",
 		data: {
-			// labels: [0, 1, 2, 3, 4, 5, 6],
-			labels: x, 
+			labels: [...Array(bg.length).keys()],
 			datasets: [
 			{
-				label: "My First dataset",
+				label: "Blood glucose level",
 				backgroundColor: "rgb(255, 99, 132)",
 				borderColor: "rgb(255, 99, 132)",
 				fill: 'False',
 				data: bg
-				// data: x
 			}
 			]
 		},
@@ -43,8 +45,9 @@
 // End copied from internet!
 </script>
 
-<h1>Your number is {}!</h1>
-<button on:click={getBg}>Get a random number</button>
+<!-- <h1>Your number is {bg}!</h1> -->
+<!-- <button on:click={getBg}>Get a random number</button> -->
 
-<button on:click={renderChart(bg)}>Load</button> 
+<h1>Press button to load standard BG values</h1>
+<button on:click={loadShow}>Load</button> 
 <canvas id="myChart"></canvas> 
